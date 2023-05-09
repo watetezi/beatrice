bb87e1049e146ae79ad6db55c1ead1af6d3247fb
Chiroxiphia caudata <musetta.alessandro@gmail.com>
2023-05-09 12:12:43 +0200

    Update UI

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-import Fullpage, { FullPageSections, FullpageSection } from '@ap.cx/react-fullpage'
+import Fullpage, { FullPageSections, FullpageSection, FullpageNavigation } from '@ap.cx/react-fullpage';
 
 export default class App extends Component {
   render() {
     return (
       <Fullpage>
 
+        <div className='dot'>
+          <FullpageNavigation style={{
+
+        }}> </FullpageNavigation>
+        </div>
+
         <FullPageSections>
 
-
           <FullpageSection style={{
-            backgroundColor: 'lime',
+            backgroundColor: 'white',
             height: '80vh',
             padding: '1em',
-          }}>1
+          }}>
+            1
           </FullpageSection>
 
           <FullpageSection style={{
-            backgroundColor: 'coral',
+            backgroundColor: 'white',
+            height: '80vh',
             padding: '1em',
           }}>
+            2
           </FullpageSection>
 
           <FullpageSection style={{
-            backgroundColor: 'firebrick',
+            backgroundColor: 'white',
+            height: '100vh',
             padding: '1em',
-          }}>
+          }}>3
           </FullpageSection>
 
         </FullPageSections>
